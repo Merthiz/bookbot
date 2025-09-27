a58ab70825c6f6b08bdaf362314d990377d9b5a9
Kaique S Silva <k16ksds@gmail.com>
2025-09-27 19:45:54 -0300

    Last changes

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import sys
-from stats import count_words, get_book_text, count_caracters, sort_on
+from stats import count_words, count_caracters, sort_on
 
 try:
     def main():
         filepath = sys.argv[1]
-        print(f"Book report of :{filepath}")
+        print("="* 20 + f" Book report of: {filepath} " + "="* 20)
         words = count_words(filepath)
         letters = count_caracters(filepath)
-        print(f"Found {words} total words")
+        print("="* 25 + f" Found {words} total words " + "="* 25)
         ordened_list = sort_on(letters)
-        print("\nCharacters count:")
+        print("="* 29 + f" Characters count: " + "="* 28)
         for item in ordened_list:
             char = item['char']
             num = item['num']
             
             if char.isalpha():
                 print(f"{char}: {num}")
+                
     main()
+    print("="* 80)
 except Exception:
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
